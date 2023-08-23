53b6653a2ea54d08c632135d2190096c5503f5c5
matheusperezz <matheus.mperez08@gmail.com>
2023-08-23 20:14:48 -0300

    4sum try in leetcode (18)

---
+++ leetcode/solutions.h
@@ -0,0 +1,21 @@
+#ifndef SOLUTIONS_H
+#define SOLUTIONS_H
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class ListNode;
+ListNode* mergeTwoLists(ListNode* list1, ListNode* list2);
+ListNode* removeNthFromEnd(ListNode* head, int n);
+vector<string> letterCombinations(string digits);
+int threeSumClosest(vector<int>& nums, int target);
+vector<vector<int>> threeSum(vector<int>& nums);
+int maxArea(vector<int>& height);
+string longestPalindrome(string s);
+double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2);
+int lengthOfLongestSubstring(string s);
+void addTwoNumbersLinkedLists(ListNode* l1, ListNode* l2);
+ListNode* reverseList(ListNode* head);
+
+#endif
