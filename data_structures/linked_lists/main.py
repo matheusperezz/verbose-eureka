bbdd107aa08e6b08e7a3e6ad186ee3e088bbb7cb
matheusperezz <matheus.mperez08@gmail.com>
2023-08-23 21:13:56 -0300

    linkedlist in python now is according with pep8

--- data_structures/linked_lists/main.py
+++ data_structures/linked_lists/main.py
@@ -3,10 +3,11 @@ class Node:
         self.data = data
         self.next = None
 
+
 class LinkedList:
     def __init__(self):
         self.head = None
-        
+
     def append(self, data):
         new_node = Node(data)
         if self.head is None:
@@ -16,13 +17,14 @@ class LinkedList:
         while current.next:
             current = current.next
         current.next = new_node
-        
+
     def display(self):
         current = self.head
         while current:
             print(current.data, end=" -> ")
             current = current.next
         print("None")
+
 
 # Creating an list
 linked_list = LinkedList()
