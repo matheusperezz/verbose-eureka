d7fde2aa5da3585b35b1b33ae652759813d0e474
matheusperezz <matheus.mperez08@gmail.com>
2023-09-09 20:15:02 -0300

    heap algorithm implemented with c++

--- data_structures/heap/main.cpp
+++ data_structures/heap/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -16,8 +17,94 @@ using namespace std;
 
 */
 
-int main(){
+class MaxHeap
+{
+private:
+    vector<int> heap;
 
-    cout << "Testing heap folder..." << endl;
+    // Function to move a element to the top of heap, and readjust
+    void heapifyUp(int index)
+    {
+        int parentIndex = (index - 1) / 2;
+        while (index > 0 and heap[index] > heap[parentIndex])
+        {
+            swap(heap[index], heap[parentIndex]);
+            index = parentIndex;
+            parentIndex = (index - 1) / 2;
+        }
+    }
+
+    // Function to move a element to the bottom of heap, and readjust
+    void heapifyDown(int index)
+    {
+        int leftChild = 2 * index + 1;
+        int rightChild = 2 * index + 2;
+        int largest = index;
+
+        if (leftChild < heap.size() and heap[leftChild] > heap[largest])
+        {
+            largest = leftChild;
+        }
+
+        if (rightChild < heap.size() and heap[rightChild] > heap[largest])
+        {
+            largest = rightChild;
+        }
+
+        if (largest != index)
+        {
+            swap(heap[index], heap[largest]);
+            heapifyDown(largest);
+        }
+    }
+
+public:
+    // Function to insert a element in heap
+    void insert(int value)
+    {
+        heap.push_back(value);
+        int index = heap.size() - 1;
+        heapifyUp(index);
+    }
+
+    // Function to remove and return the max element of heap
+    int extractMax()
+    {
+        if (heap.empty())
+        {
+            throw runtime_error("Heap vazio");
+        }
+
+        int max = heap[0];
+        heap[0] = heap.back(); // reference to the last item in vector
+        heap.pop_back();
+        heapifyDown(0);
+
+        return max;
+    }
+
+    // Function to verify if heap is empty
+    bool isEmpty() const
+    {
+        return heap.empty();
+    }
+};
+
+int main()
+{
+    MaxHeap maxHeap;
+
+    maxHeap.insert(12);
+    maxHeap.insert(8);
+    maxHeap.insert(15);
+    maxHeap.insert(21);
+
+    while (!maxHeap.isEmpty())
+    {
+        cout << maxHeap.extractMax() << " ";
+    }
+
+    cout << endl;
+
     return 0;
 }
