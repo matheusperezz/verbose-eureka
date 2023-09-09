673047e60a0e8dccf2b21574c4b1c210af2fd353
matheusperezz <matheus.mperez08@gmail.com>
2023-09-09 01:12:14 -0300

    setuping the repository on ubuntu

--- leetcode/main.cpp
+++ leetcode/main.cpp
@@ -8,36 +8,41 @@
 /*
  *      Para compilar o código, execute os seguintes comandos:
  *      g++ solutions.cpp main.cpp -o main.exe
- *      .\main.exe 
-*/
+ *      .\main.exe
+ */
 
 using namespace std;
 
-class ListNode {
-    public:
-        int val;
-        ListNode* next;
+class ListNode
+{
+public:
+    int val;
+    ListNode *next;
 
-        // Construtores
-        ListNode(){
-            val = 0;
-            next = nullptr;
-        }
+    // Construtores
+    ListNode()
+    {
+        val = 0;
+        next = nullptr;
+    }
 
-        ListNode(int val){
-            this->val = val;
-            next = nullptr;
-        }
+    ListNode(int val)
+    {
+        this->val = val;
+        next = nullptr;
+    }
 
-        ListNode(int val, ListNode* next){
-            this->val = val;
-            this->next = next;
-        }
+    ListNode(int val, ListNode *next)
+    {
+        this->val = val;
+        this->next = next;
+    }
 };
 
-int main(){
+int main()
+{
 
-    
+    cout << "testando" << endl;
 
     return 0;
 }
