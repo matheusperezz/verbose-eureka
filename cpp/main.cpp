b736896dfd06b9bf1c45c4aca8f3876c7fa65623
matheusperezz <matheus.mperez08@gmail.com>
2023-10-14 15:57:28 -0300

    adding the running instructions in wsl

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -3,6 +3,14 @@
 
 using namespace std;
 
+/**
+ *  To run the code do this steps:
+ *  1 - Open the cpp folder in wsl
+ *  2 - Run: "make && ./cpp"
+ *
+ * @return
+ */
+
 int main() {
     solution_111();
     return 0;
