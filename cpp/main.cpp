1707d7b52dfc9f675b4e65b358f47ed5601eddf3
matheusperezz <matheus.mperez08@gmail.com>
2023-10-18 20:52:24 -0300

    remember how hash works

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include "leetcode.h"
 
 using namespace std;
@@ -11,7 +12,30 @@ using namespace std;
  * @return
  */
 
+int hashash(string value){
+    int hashValue = 0;
+    int capacity = 10;
+
+    for (char c : value){
+        hashValue = ( hashValue * 31 + c) % capacity;
+    }
+
+    return hashValue;
+}
+
 int main() {
-    solution_119();
+    // solution_119();
+    vector<string> v = {"banana", "apple", "renan", "miwis"};
+    int arr[20];
+
+    for (auto &e : v){
+        int r = hashash(e);
+        arr[r] = 2;
+    }
+
+    for (auto &c : arr)
+        cout << c << " ";
+
+    cout << endl;
     return 0;
 }
