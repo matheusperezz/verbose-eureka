b8aaf82d1dc3b6ddede1768018c5dc29493e8d50
matheusperezz <matheus.mperez08@gmail.com>
2023-12-15 15:15:05 -0300

    adding python folder

--- kotlin2/src/main/kotlin/Main.kt
+++ kotlin2/src/main/kotlin/Main.kt
@@ -1,8 +1,3 @@
-import leetcode.Solution228
-
-fun main() {
-    val s = Solution228()
-    val nums: IntArray = intArrayOf(1, 2, 3, 5, 6, 7, 9)
-    val output = s.summaryRanges(nums)
-    println(output)
+fun main(){
+    
 }
