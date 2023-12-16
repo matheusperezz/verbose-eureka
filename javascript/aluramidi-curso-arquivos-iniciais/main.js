d852d587cf06ae759e20ce73734ce36b83d37f3c
matheusperezz <matheus.mperez08@gmail.com>
2023-12-16 11:27:48 -0300

    finish the javascript project from alura course

--- javascript/aluramidi-curso-arquivos-iniciais/main.js
+++ javascript/aluramidi-curso-arquivos-iniciais/main.js
@@ -1,6 +1,5 @@
 function tocarSom(idElemento){
     let texto = `#som_${idElemento.toLowerCase()}`
-    console.log(texto)
     document.querySelector(texto).play();
 }
 
@@ -8,6 +7,16 @@ const listaTeclas = document.querySelectorAll('.tecla');
 
 listaTeclas.forEach((botao) => {
     botao.onclick = function () {
-        tocarSom(botao.classList[1])
+        tocarSom(botao.classList[1]);
     };
+
+    botao.onkeydown = function (event) {
+        if (event.key === 'Enter' || event.key === 'Space '){
+            botao.classList.add('ativa');
+        }
+    }
+
+    botao.onkeyup = function () {
+        botao.classList.remove('ativa');
+    }
 })
