3ba402449bc2b7d39fdb2a5e1d10c9bcbd4bb73a
matheusperezz <matheus.mperez08@gmail.com>
2023-12-17 15:32:35 -0300

    fokus project finished

--- javascript/Fokus-projeto-base/main.js
+++ javascript/Fokus-projeto-base/main.js
@@ -5,10 +5,26 @@ const longoBt = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
 const botoes = document.querySelectorAll('.app__card-button')
+const checkboxMusica = document.querySelector('#alternar-musica');
 const startPauseBt = document.querySelector('#start-pause');
+const startPauseTexto = document.querySelector('#start-pause span');
+const startPauseImage = document.querySelector('.app__card-primary-butto-icon');
+const timer = document.querySelector('#timer')
 
-let tempoDecorridoEmSegundos = 5;
+// Foco 25min
+let tempoDecorridoEmSegundos = 25 * 60;
 let intervaloId = null
+let emContagem = false
+
+/** Audios **/
+const musicaLuna = new Audio('./sons/luna-rise-part-one.mp3');
+const musicaBeep = new Audio('./sons/beep.mp3');
+const musicaPlay = new Audio('./sons/play.wav');
+const musicaPause = new Audio('./sons/pause.mp3');
+
+checkboxMusica.addEventListener('change', () => {
+    tocarMusica(musicaLuna)
+})
 
 focoBt.addEventListener('click', () => {
     alterarContexto('foco');
@@ -39,18 +55,24 @@ function alterarContexto(contexto) {
             Otimize sua produtividade,<br>
                 <strong class="app__title-strong">mergulhe no que importa.</strong>
             `
+            tempoDecorridoEmSegundos = 25 * 60
+            mudarTimer(tempoDecorridoEmSegundos)
             break;
         case 'descanso-curto':
             titulo.innerHTML = `
             Que tal dar uma respirada?<br>
                 <strong class="app__title-strong">Faça uma pausa curta!</strong>
             `
+            tempoDecorridoEmSegundos = 5 * 60
+            mudarTimer(tempoDecorridoEmSegundos)
             break;
         case 'descanso-longo':
             titulo.innerHTML = `
             Hora de voltar à superfície.<br>
                 <strong class="app__title-strong"> Faça uma pausa longa.</strong>
             `
+            tempoDecorridoEmSegundos = 10 * 60
+            mudarTimer(tempoDecorridoEmSegundos)
             break;
     }
 
@@ -59,23 +81,56 @@ function alterarContexto(contexto) {
 const contagemRegressiva = () => {
     if (tempoDecorridoEmSegundos <= 0){
         zerar();
+        // musicaBeep.play();
         alert('Tempo finalizado');
         return;
     }
     tempoDecorridoEmSegundos -= 1;
-    console.log(`Temporizador: ${tempoDecorridoEmSegundos}`);
+    mudarTimer(tempoDecorridoEmSegundos)
 }
 
-startPauseBt.addEventListener('click', iniciar);
+startPauseBt.addEventListener('click', toggleIntervalo);
 
-function iniciar(){
+function toggleIntervalo(){
+
     if (intervaloId){
         zerar()
     }
-    intervaloId = setInterval(contagemRegressiva, 1000);
+    if (emContagem){
+        // Pausou
+        musicaPause.play()
+        startPauseImage.setAttribute('src', './imagens/play_arrow.png')
+        startPauseTexto.textContent = 'Começar'
+        clearInterval(intervaloId)
+    } else {
+        // Ativou o contador
+        startPauseTexto.textContent = 'Pausar'
+        startPauseImage.setAttribute('src', './imagens/pause.png')
+        musicaPlay.play()
+        intervaloId = setInterval(contagemRegressiva, 1000);
+    }
+
+    emContagem = !emContagem
 }
 
 function zerar() {
     clearInterval(intervaloId);
     intervaloId = null;
+}
+
+function tocarMusica(musica){
+    if (musica.paused){
+        musica.play()
+    } else {
+        musica.pause()
+    }
+}
+function mudarTimer(tempoEmSegundos){
+    let minuto = Math.floor(tempoEmSegundos / 60)
+    let segundo = Math.floor(tempoEmSegundos % 60)
+    if (segundo === 0){
+        timer.textContent = `${minuto}:00`
+    } else {
+        timer.textContent = `${minuto}:${segundo}`
+    }
 }
