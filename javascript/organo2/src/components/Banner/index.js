25ecd2ed2f25605f1d4f8258c1753b9a53dd6b2d
matheusperezz <matheus.mperez08@gmail.com>
2023-12-19 15:45:43 -0300

    remove: organo2 project

--- javascript/organo2/src/components/Banner/index.js
+++
@@ -1,9 +0,0 @@
-import './banner.css'
-
-const Banner = () => {
-    return (<header className='cabecalho'>
-        <img src='/imagens/banner.png' alt='Logo do organo'/>
-    </header>)
-}
-
-export default Banner
