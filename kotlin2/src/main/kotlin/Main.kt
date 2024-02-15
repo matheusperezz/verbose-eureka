1e996171f63f6ab7d339fc821ba58d62cc7696c6
matheusperezz <matheus.mperez08@gmail.com>
2024-02-15 08:31:14 -0300

    leetcode 33 finished with kotlin

--- kotlin2/src/main/kotlin/Main.kt
+++ kotlin2/src/main/kotlin/Main.kt
@@ -1,10 +1,10 @@
-import data.ListNode
-import leetcodeMedium.Solution24
-import leetcodeMedium.Solution29
+import leetcodeMedium.Solution33
 
 fun main(){
-    val s = Solution29()
-    val output = s.divide(-2147483648, -1)
+    val s = Solution33()
+    val arr: IntArray = intArrayOf(4,5,6,7,0,1,2)
+    val target = 0
+    val output = s.search(arr, target)
     println(output)
 
 }
