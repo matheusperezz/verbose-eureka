f5d48000bf163aa6d37db849fed9aeb270398339
matheusperezz <matheus.mperez08@gmail.com>
2024-02-28 17:06:30 -0300

    delete 49 because its 48, now add leetcode 49 finished with python

--- python/49.py
+++ python/49.py
@@ -1,48 +1,21 @@
+from itertools import permutations
+
 class Solution:
-    def rotate(self, matrix: list[list[int]]) -> None:
-        n_col = len(matrix[0])
-        cols = [[] for _ in range(n_col)]
+    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+        anagram_group = {}
 
-        for row in matrix:
-            for idx, element in enumerate(row):
-                cols[idx].insert(0, element)
+        for s in strs:
+            sorted_s = ''.join(sorted(s))
 
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                matrix[i][j] = cols[i][j]
+            if sorted_s in anagram_group:
+                anagram_group[sorted_s].append(s)
+            else:
+                anagram_group[sorted_s] = [s]
 
-    def rotate2(self, matrix: list[list[int]]) -> None:
-        n = len(matrix)
-
-        for i in range(n):
-            for j in range(i, n):
-                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-
-        for row in matrix:
-            row.reverse()
+        return list(anagram_group.values())
 
 
 
 s = Solution()
-matrix = [
-    [1,2,3],
-    [4,5,6],
-    [7,8,9]
-]
-'''
-[1 2 3]     [7 4 1]
-[4 5 6]     [8 5 2]
-[7 8 9]     [9 6 3]
-
-'''
-s.rotate2(matrix)
-print(matrix)
-
-matrix = [
-    [5,1,9,11],
-    [2,4,8,10],
-    [13,3,6,7],
-    [15,14,12,16]
-]
-s.rotate2(matrix)
-print(matrix)
+strs = ["eat","tea","tan","ate","nat","bat"]
+print(s.groupAnagrams(strs))
