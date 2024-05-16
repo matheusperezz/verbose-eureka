40f40bccbaa42ca5601ac23859c8ba89dea9b5d4
matheusperezz <matheus.mperez08@gmail.com>
2024-05-16 11:30:59 -0300

    minheap implemented

--- data_structures/heap/heap.py
+++ data_structures/heap/heap.py
@@ -1,3 +1,72 @@
+class MinHeap:
+    def __init__(self) -> None:
+        self.heap = []
+    
+    def _parent(self, index):
+        return (index - 1) // 2
+    
+    def _left_child(self, index):
+        return 2 * index + 1
+    
+    def _right_child(self, index):
+        return 2 * index + 2
+    
+    def _has_left_child(self, index):
+        return self._left_child(index) < len(self.heap)
+    
+    def _has_right_child(self, index):
+        return self._right_child(index) < len(self.heap)
+    
+    def _swap(self, i, j):
+        self.heap[i], self.heap[j] = self.heap[j], self.heap[i]
+    
+    def _heapify_up(self, index):
+        parent = self._parent(index)
+        if index > 0 and self.heap[index] < self.heap[parent]:
+            self._swap(index, parent)
+            self._heapify_up(parent)
+    
+    def _heapify_down(self, index):
+        smallest = index
+        left = self._left_child(index)
+        right = self._right_child(index)
+
+        if self._has_left_child(index) and self.heap[left] < self.heap[smallest]:
+            smallest = left
+        
+        if self._has_right_child(index) and self.heap[right] < self.heap[smallest]:
+            smallest = right
+        
+        if smallest != index:
+            self._swap(index, smallest)
+            self._heapify_down(smallest)
+    
+    def insert(self, item):
+        self.heap.append(item)
+        self._heapify_up(len(self.heap) - 1)
+
+    def extract_min(self):
+        if len(self.heap) == 0:
+            raise IndexError("extract_min from an empty heap")
+        if len(self.heap) == 1:
+            return self.heap.pop()
+        root = self.heap[0]
+        self.heap[0] = self.heap.pop()
+        self._heapify_down(0)
+        return root
+    
+    def peek_min(self):
+        if len(self.heap) == 0:
+            raise IndexError("peek_min from an empty heap")
+        return self.heap[0]
+    
+    def size(self):
+        return len(self.heap)
+    
+    def is_empty(self):
+        return len(self.heap) == 0
+        
+
 class MaxHeap:
     def __init__(self):
         self.heap = []
