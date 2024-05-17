a0fd37ac09537c4e13c6a2468c6a7ac385b550e7
matheusperezz <matheus.mperez08@gmail.com>
2024-05-17 15:49:24 -0300

    preorder, inorder, postorder and levelorder in trees

--- data_structures/tree/tree.py
+++ data_structures/tree/tree.py
@@ -4,6 +4,43 @@ class TreeNode:
         self.left = left
         self.right = right
 
+    def preorder(self, node):
+        if node is None:
+            return
+        print(node.val, end=' ')
+        self.preorder(node.left)
+        self.preorder(node.right)
+    
+    def inorder(self, node):
+        if node is None:
+            return
+        self.inorder(node.left)
+        print(node.val, end=' ')
+        self.inorder(node.right)
+    
+    def postorder(self, node):
+        if node is None:
+            return
+        self.postorder(node.left)
+        self.postorder(node.right)
+        print(node.val, end=' ')
+    
+    def levelorder(self, root):
+        if root is None:
+            return
+        
+        queue = []
+        queue.append(root)
+        while queue:
+            node = queue.pop(0)
+            print(node.val, end=' ')
+
+            if node.left is not None:
+                queue.append(node.left)
+            if node.right is not None:
+                queue.append(node.right)
+    
+
 def printTree(root):
     if not root: return
     print(root.val, end=' ')
@@ -23,5 +60,21 @@ def insertRecursive(node, val):
     
 root = TreeNode(80)
 insertRecursive(root, 99)
-insertRecursive(root, 1)
-printTree(root)
+insertRecursive(root, 3)
+insertRecursive(root, 98)
+insertRecursive(root, -23)
+insertRecursive(root, 5)
+insertRecursive(root, 202)
+insertRecursive(root, 0)
+print("Preorder")
+root.preorder(root)
+print()
+print("Inorder")
+root.inorder(root)
+print()
+print("Postorder")
+root.postorder(root)
+print()
+print("Levelorder")
+root.levelorder(root)
+print()
