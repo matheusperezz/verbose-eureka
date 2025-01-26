2922f5b5126b3bc9aaae7f6f06dbacd6a7d5864f
matheusperezz <matheus.mperez08@gmail.com>
2025-01-25 23:37:09 -0300

    add: problem median of two sorted arrays

--- leetcode/main.cpp
+++ leetcode/main.cpp
@@ -1,10 +1,11 @@
-#include <iostream>
-#include "solutions.h"
-#include <string>
 #include <algorithm>
-#include <cstdlib>
 #include <climits>
+#include <cstdlib>
+#include <iostream>
+#include <string>
 #include <unordered_map>
+#include <vector>
+
 /*
  *      Para compilar o código, execute os seguintes comandos:
  *      g++ solutions.cpp main.cpp -o main.exe
@@ -13,36 +14,68 @@
 
 using namespace std;
 
-class ListNode
-{
+class Solution {
 public:
-    int val;
-    ListNode *next;
-
-    // Construtores
-    ListNode()
-    {
-        val = 0;
-        next = nullptr;
-    }
-
-    ListNode(int val)
-    {
-        this->val = val;
-        next = nullptr;
-    }
-
-    ListNode(int val, ListNode *next)
-    {
-        this->val = val;
-        this->next = next;
+  double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2) {
+        int n1 = nums1.size(), n2 = nums2.size();
+        
+        if (n1 > n2)
+            return findMedianSortedArrays(nums2, nums1);
+        
+        int n = n1 + n2;
+        int left = (n1 + n2 + 1) / 2; // Calculate the left partition size
+        int low = 0, high = n1;
+        
+        while (low <= high) {
+            int mid1 = (low + high) >> 1; // Calculate mid index for nums1
+            int mid2 = left - mid1; // Calculate mid index for nums2
+            
+            int l1 = INT_MIN, l2 = INT_MIN, r1 = INT_MAX, r2 = INT_MAX;
+            
+            // Determine values of l1, l2, r1, and r2
+            if (mid1 < n1)
+                r1 = nums1[mid1];
+            if (mid2 < n2)
+                r2 = nums2[mid2];
+            if (mid1 - 1 >= 0)
+                l1 = nums1[mid1 - 1];
+            if (mid2 - 1 >= 0)
+                l2 = nums2[mid2 - 1];
+            
+            if (l1 <= r2 && l2 <= r1) {
+                // The partition is correct, we found the median
+                if (n % 2 == 1)
+                    return max(l1, l2);
+                else
+                    return ((double)(max(l1, l2) + min(r1, r2))) / 2.0;
+            }
+            else if (l1 > r2) {
+                // Move towards the left side of nums1
+                high = mid1 - 1;
+            }
+            else {
+                // Move towards the right side of nums1
+                low = mid1 + 1;
+            }
+        }
+        
+        return 0; // If the code reaches here, the input arrays were not sorted.
     }
 };
 
-int main()
-{
+int main() {
+  vector<int> nums1;
+  vector<int> nums2;
 
-    cout << "testando" << endl;
+  nums1.push_back(1);
+  nums1.push_back(3);
 
-    return 0;
+  nums2.push_back(2);
+
+  Solution s;
+  double output = s.findMedianSortedArrays(nums1, nums2);
+
+  cout << "output: " << output << endl;
+
+  return 0;
 }
